8ddb130456063bd5da7685cbbdbcb3ad00160161
LautiGR_116 <lreinoso@mail.austral.edu.ar>
2024-11-21 18:53:24 -0300

    improve utils logging

--- src/main/kotlin/edu/ingsis/printscriptService/utils/QueueInputProvider.kt
+++ src/main/kotlin/edu/ingsis/printscriptService/utils/QueueInputProvider.kt
@@ -1,10 +1,19 @@
 package edu.ingsis.printscriptService.utils
 
 import lib.InputProvider
+import org.slf4j.LoggerFactory
 import java.util.Queue
 
 class QueueInputProvider(private val queue: Queue<String>) : InputProvider {
+    private val logger = LoggerFactory.getLogger(QueueInputProvider::class.java)
+
     override fun input(message: String): String {
-        return queue.poll()
+        val value = queue.poll()
+        if (value != null) {
+            logger.info("Polled value from queue: $value")
+        } else {
+            logger.warn("Queue is empty when attempting to poll")
+        }
+        return value ?: ""
     }
 }
--- src/main/kotlin/edu/ingsis/printscriptService/utils/ValidateErrorHandler.kt
+++ src/main/kotlin/edu/ingsis/printscriptService/utils/ValidateErrorHandler.kt
@@ -1,20 +1,25 @@
 package edu.ingsis.printscriptService.utils
 
 import edu.ingsis.printscriptService.dto.ErrorDTO
+import org.slf4j.LoggerFactory
 import runner.ErrorHandler
 
 class ValidateErrorHandler : ErrorHandler {
     private val errors = mutableListOf<ErrorDTO>()
+    private val logger = LoggerFactory.getLogger(ValidateErrorHandler::class.java)
 
     override fun handleError(message: String) {
+        logger.error("Error encountered: $message")
         errors.add(ErrorDTO(message))
     }
 
     fun cleanErrors() {
+        logger.info("Cleaning all errors")
         errors.clear()
     }
 
     fun getErrors(): List<ErrorDTO> {
+        logger.debug("Retrieving ${errors.size} errors")
         return errors
     }
 }
