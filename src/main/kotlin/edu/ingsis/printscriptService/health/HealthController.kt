31047d67e378f87af5243ef4b1749c78734834ff
LautiGR_116 <lreinoso@mail.austral.edu.ar>
2024-11-21 18:07:55 -0300

    improve health logging

--- src/main/kotlin/edu/ingsis/printscriptService/health/HealthController.kt
+++ src/main/kotlin/edu/ingsis/printscriptService/health/HealthController.kt
@@ -4,11 +4,18 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Level
+import java.util.logging.Logger
 
 @RestController
 @RequestMapping("/health")
 class HealthController {
 
+    private val logger: Logger = Logger.getLogger(HealthController::class.java.name)
+
     @GetMapping("")
-    fun ping(): ResponseEntity<Unit> = ResponseEntity.ok().build()
+    fun ping(): ResponseEntity<Unit> {
+        logger.log(Level.FINE, "Health check received")
+        return ResponseEntity.ok().build()
+    }
 }
